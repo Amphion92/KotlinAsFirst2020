d03a186c9824f04287936e49ac3231fd34a8e3c6
Малыш Макар <makar_malish@mail.ru>
2020-09-10 12:49:07 +0300

    Исправлена функция accountInThreeYears

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -65,7 +65,7 @@ fun main() {
  * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
  * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
  */
-fun seconds(hours: Int, minutes: Int, seconds: Int): Int = hours*3600+minutes*60+seconds
+fun seconds(hours: Int, minutes: Int, seconds: Int): Int = hours * 3600 + minutes * 60 + seconds
 
 /**
  * Тривиальная (1 балл)
@@ -74,7 +74,8 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = hours*3600+minutes*60
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = ((sagenes*48+arshins*16+vershoks)*4.445)/100
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double =
+    ((sagenes * 48 + arshins * 16 + vershoks) * 4.445) / 100
 
 /**
  * Тривиальная (1 балл)
@@ -83,8 +84,8 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = ((sagene
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
 fun angleInRadian(deg: Int, min: Int, sec: Int): Double {
-    val allsec: Int = (deg*3600+min*60+sec)
-    val rad: Double = (allsec*PI)/648000
+    val allsec: Int = (deg * 3600 + min * 60 + sec)
+    val rad: Double = (allsec * PI) / 648000
     return rad
 
 }
@@ -95,7 +96,8 @@ fun angleInRadian(deg: Int, min: Int, sec: Int): Double {
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqrt((x2-x1) *(x2-x1)+(y2-y1)*(y2-y1))
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double =
+    sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))
 
 /**
  * Простая (2 балла)
@@ -103,7 +105,7 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqrt((
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int = (number%1000)/100
+fun thirdDigit(number: Int): Int = (number % 1000) / 100
 
 /**
  * Простая (2 балла)
@@ -113,7 +115,7 @@ fun thirdDigit(number: Int): Int = (number%1000)/100
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
 fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int {
-    return (hoursArrive*60+minutesArrive)-(hoursDepart*60+minutesDepart)
+    return (hoursArrive * 60 + minutesArrive) - (hoursDepart * 60 + minutesDepart)
 }
 
 /**
@@ -123,12 +125,12 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double {
-    var x: Double = (initial+(initial*percent)/100).toDouble()
-    x = (x+(x*percent)/100)
-    x = (x+(x*percent)/100)
+fun accountInThreeYears(initial: Int, percent: Int): Double { //Переделать
+    var x: Double = (initial + (initial * percent) / 100.0)
+    x = x + (x * percent) / 100
+    x = x + (x * percent) / 100
     return x
-    }
+}
 
 /**
  * Простая (2 балла)
@@ -137,8 +139,8 @@ fun accountInThreeYears(initial: Int, percent: Int): Double {
  * Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
 fun numberRevert(number: Int): Int {
-    var revnum : Double= ((number%10)*100).toDouble()
-    revnum = (((number%100)/10)*10)+revnum
-    revnum = (number/100)+revnum
+    var revnum: Double = ((number % 10) * 100).toDouble()
+    revnum = (((number % 100) / 10) * 10) + revnum
+    revnum = (number / 100) + revnum
     return revnum.toInt()
 }
